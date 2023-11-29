af66e0117a6a7ea6c7c116a6a05a704037b83819
eunjeong Kim <guri2913@gmail.com>
2023-11-29 13:04:47 +0900

    메인 화면 수정

--- src/main/java/com/example/kicktheworld_test/MainController.java
+++ src/main/java/com/example/kicktheworld_test/MainController.java
@@ -1,17 +1,30 @@
 package com.example.kicktheworld_test;
 
+
+import com.example.kicktheworld_test.Trip.Dto.MainItemDto;
+import com.example.kicktheworld_test.Trip.Dto.TripSearchDto;
+import com.example.kicktheworld_test.Trip.Service.TripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
-    @GetMapping("/")
-    public String mainPage() {
-        // 여행지 소개를 클릭하면 메인 페이지로 이동하는 뷰 이름을 반환합니다.
-        return "mainPage";
-    }
+
+
     @GetMapping("/admin")
     public String adminPage() {
         return "adminPage";
     }
+
+
 }
